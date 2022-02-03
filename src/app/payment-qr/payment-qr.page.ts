ddfc70fe6739d4962297c11c05ed4ba2a0ef2e49
tachapong <tachapong@perfenterprise.com>
2022-02-03 16:28:23 +0700

    Implement download image.

--- src/app/payment-qr/payment-qr.page.ts
+++ src/app/payment-qr/payment-qr.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Share } from '@capacitor/share';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-payment-qr',
@@ -11,6 +12,27 @@ export class PaymentQRPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectDownload() {
+
+    var element = document.getElementById('payment-qr-content');
+    htmlToImage.toPng(element)
+      .then(function (dataUrl) {
+        download("payment-" + Math.random().toString().slice(2, 7), dataUrl);
+      })
+      .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+      });
+
+    function download(filename, data) {
+      var element = document.createElement('a');
+      element.setAttribute('href', data);
+      element.setAttribute('download', filename);
+      document.body.appendChild(element);
+      element.click();
+    }
+
   }
 
   async onSelectShare() {
